0c822a808ac771bb7241981aa340a770ff4dceeb
yiliu-mt <yi.liu@mthreads.com>
2023-01-05 09:11:18 +0000

    update 2023Q1/0103

--- exp/2023Q1/0103/prepare.sh
+++ exp/2023Q1/0103/prepare.sh
@@ -1,5 +1,4 @@
-docker run --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace/vits -v /nfs1:/nfs1 -v /nfs2:/nfs2 -v /nfs1/yichao.hu:/data -v /home/yichao.hu/nltk_data:/root/nltk_data -v /nfs2/yi.liu/data:/data2 -v /nfs1/yi.liu/tts:/data3 -it mycrazycracy/wetts:1.0 bash
-
+docker run --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace/vits -v /nfs1:/nfs1 -v /nfs2:/nfs2 -v /data:/data -it mycrazycracy/wetts:1.0 bash
 
 # with jams
 python tools/generate_vits_data.py \
@@ -11,6 +10,7 @@ python tools/generate_vits_data.py \
     -p exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022_Jams1228/preprocess.yaml \
     -o exp/2023Q1/0103/preprocessed_data/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022_Jams1228
 python gen_spec.py exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022_Jams1228.json
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train_ms.py -c exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022_Jams1228.json -m Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022_Jams1228
 
 
 # without jams
@@ -23,4 +23,5 @@ python tools/generate_vits_data.py \
     -p exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/preprocess.yaml \
     -o exp/2023Q1/0103/preprocessed_data/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022
 python gen_spec.py exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022.json
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_ms.py -c exp/2023Q1/0103/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022.json -m Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022
 

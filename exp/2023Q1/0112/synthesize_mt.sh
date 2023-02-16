4f19148d9839b1173c9e58f46588214d5399dc3d
Yi Liu <yi.liu@mthreads.com>
2023-02-16 02:53:39 +0000

    add mtgpu speed test

--- exp/2023Q1/0112/synthesize_mt.sh
+++ exp/2023Q1/0112/synthesize_mt.sh
@@ -1,7 +1,13 @@
 config=exp/2023Q1/0112/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/tuning_mita_bc_chatbot10.json
-test_file=exp/2023Q1/0112/test.txt
 model=model/G_200000.pth
-output_dir=exp/2023Q1/0112/audio_out
-
+output_dir=exp/2023Q1/0112/audio_out/mt_test
 mkdir -p $output_dir
-CUDA_VISIBLE_DEVICES=0 python inference_ms.py -c $config -t $test_file -m $model -o $output_dir
+
+# # To test the speed
+# test_file=exp/2023Q1/0103/preprocessed_data/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/test_mita.txt
+# PVR_GPUIDX=4 python inference_ms.py --warmup --repeat 10 -c $config -t $test_file -m $model -o $output_dir
+
+# numeric accuracy
+test_file=exp/2023Q1/0112/test.txt
+PVR_GPUIDX=4 python inference_ms.py -c $config -t $test_file -m $model -o $output_dir
+
--- inference_ms.py
+++ inference_ms.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import time
 import torch
 import musa_torch_extension
 import numpy as np
@@ -20,6 +21,8 @@ device = torch.device("mtgpu")
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--warmup", action="store_true")
+    parser.add_argument("--repeat", type=int, default=1)
     parser.add_argument("-c", "--config", help="config file")
     parser.add_argument("-t", "--test_file", help="test file")
     parser.add_argument("-m", "--model", help="model file")
@@ -40,23 +43,48 @@ def main():
     print(f"Loaded model from {args.model}")
 
     audiopaths_sid_text = utils.load_filepaths_and_text(args.test_file)
-    with torch.no_grad():
-        for audiopath, sid, text in audiopaths_sid_text:
-            print(audiopath)
-            text_norm = cleaned_text_to_sequence(text, hps.data.get("symbol_version", "default"))
-            if hps.data.add_blank:
-                text_norm = commons.intersperse(text_norm, 0)
-            text_padded = torch.LongTensor(text_norm)
-            x_tst = text_padded.to(device).unsqueeze(0)
-            x_tst_lengths = torch.LongTensor([text_padded.size(0)]).to(device)
-            sid = torch.LongTensor([int(sid)]).to(device)
 
-            audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
-            audio *= 32767 / max(0.01, np.max(np.abs(audio))) * 0.6
-            audio = np.clip(audio, -32767.0, 32767.0)
-            wav_name = audiopath.split("/")[-1]
-            wavfile.write(os.path.join(args.output_dir, wav_name),
-                          hps.data.sampling_rate, audio.astype(np.int16))
+    if args.warmup:
+        print("warm up...")
+        with torch.no_grad():
+            for audiopath, sid, text in audiopaths_sid_text:
+                text_norm = cleaned_text_to_sequence(text, hps.data.get("symbol_version", "default"))
+                if hps.data.add_blank:
+                    text_norm = commons.intersperse(text_norm, 0)
+                text_padded = torch.LongTensor(text_norm)
+                x_tst = text_padded.to(device).unsqueeze(0)
+                x_tst_lengths = torch.LongTensor([text_padded.size(0)]).to(device)
+                sid = torch.LongTensor([int(sid)]).to(device)
+                audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+
+    total_used_time = 0
+    total_generated_time = 0
+    with torch.no_grad():
+        for _ in range(args.repeat):
+            for audiopath, sid, text in audiopaths_sid_text:
+                print(audiopath)
+                text_norm = cleaned_text_to_sequence(text, hps.data.get("symbol_version", "default"))
+                if hps.data.add_blank:
+                    text_norm = commons.intersperse(text_norm, 0)
+                text_padded = torch.LongTensor(text_norm)
+                x_tst = text_padded.to(device).unsqueeze(0)
+                x_tst_lengths = torch.LongTensor([text_padded.size(0)]).to(device)
+                sid = torch.LongTensor([int(sid)]).to(device)
+
+                start_time = time.time()
+                audio = net_g.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667, noise_scale_w=0.8, length_scale=1)[0][0,0].data.cpu().float().numpy()
+                total_used_time += time.time() - start_time
+                total_generated_time += audio.shape[0] / hps.data.sampling_rate
+
+                audio *= 32767 / max(0.01, np.max(np.abs(audio))) * 0.6
+                audio = np.clip(audio, -32767.0, 32767.0)
+                wav_name = audiopath.split("/")[-1]
+                wavfile.write(os.path.join(args.output_dir, wav_name),
+                              hps.data.sampling_rate, audio.astype(np.int16))
+
+    print(f"Total used time: {total_used_time / args.repeat}")
+    print(f"Total synthesized time: {total_generated_time / args.repeat}")
+    print(f"RTFX: {total_used_time / total_generated_time}")
 
 
 if __name__ == "__main__":

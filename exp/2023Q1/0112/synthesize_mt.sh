5e6357a2f3a79f5a78c20c1f6dadc79d100bf176
Yi Liu <yi.liu@mthreads.com>
2023-02-16 10:45:14 +0800

    add speed test for inference

--- exp/2023Q1/0112/synthesize_mt.sh
+++ exp/2023Q1/0112/synthesize_mt.sh
@@ -1,7 +1,16 @@
 config=exp/2023Q1/0112/configs/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/tuning_mita_bc_chatbot10.json
-test_file=exp/2023Q1/0112/test.txt
 model=/nfs1/yi.liu/tts/vits/exp/2023Q1/0112/tuning_mita_bc_chatbot10/G_200000.pth
-output_dir=exp/2023Q1/0112/audio_out
-
+output_dir=exp/2023Q1/0112/audio_out/mt_test
 mkdir -p $output_dir
-CUDA_VISIBLE_DEVICES=0 python inference_ms.py -c $config -t $test_file -m $model -o $output_dir
+
+# # To test the speed
+# test_file=exp/2023Q1/0103/preprocessed_data/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/test_mita.txt
+# CUDA_VISIBLE_DEVICES=5 python inference_ms.py --warmup --repeat 10 -c $config -t $test_file -m $model -o $output_dir
+
+# # CPU
+# test_file=exp/2023Q1/0103/preprocessed_data/Baker_LJSpeech_MuSha0914_RxEnhancedV5_AISHELL3_Mita1022/test_mita.txt
+# CUDA_VISIBLE_DEVICES= python inference_ms.py --warmup --repeat 10 -c $config -t $test_file -m $model -o $output_dir
+
+# To test the accuracy
+test_file=exp/2023Q1/0112/test.txt
+CUDA_VISIBLE_DEVICES= python inference_ms.py -c $config -t $test_file -m $model -o $output_dir

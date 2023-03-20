c932982e06683441551297c429c7e83488abca6a
Yi Liu <yi.liu@mthreads.com>
2023-03-20 19:28:12 +0800

    add new exp for v0

--- exp/2023Q1/jams_v0/0308/inference_train_jams.sh
+++ exp/2023Q1/jams_v0/0308/inference_train_jams.sh
@@ -1,15 +1,15 @@
 # docker run --rm --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v $PWD:/workspace/vits -v /home/ubuntu:/home/ubuntu -v /nfs1:/nfs1 -v /nfs2:/nfs2 -it mycrazycracy/vits:v1.0 bash
 
-config=logs/exp/2023Q1/jams_v0/0308/train_jams_rx/config.json
-model=logs/exp/2023Q1/jams_v0/0308/train_jams_rx/G_120000.pth
+config=/nfs1/yi.liu/tts/vits/exp/2023Q1/jams_v0/0308/train_jams_rx_full/config.json
+model=/nfs1/yi.liu/tts/vits/exp/2023Q1/jams_v0/0308/train_jams_rx_full/G_100000.pth
 lexicon=lexicon/v1.9/bilingual_serving_dict_er3.txt
 speaker_id=0
 
-test_data=testdata/jams/new_year.txt
-output_dir=exp/2023Q1/jams_v0/0308/audio_out/new_year
+test_data=testdata/jams/jams_test_ssmlv2.txt
+output_dir=/nfs1/yi.liu/tts/vits/exp/2023Q1/jams_v0/0308/train_jams_rx_full/jams_test_ssmlv2
 
 export PYTHONPATH=.
-CUDA_VISIBLE_DEVICES=0 python synthesize_v2.py \
+CUDA_VISIBLE_DEVICES=6 python synthesize_v2.py \
     -c $config \
     -m $model \
     -t $test_data \

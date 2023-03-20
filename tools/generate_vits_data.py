cfe61bcfd57abc9acfb9ebaf36e5b7126e70d45e
Yi Liu <yi.liu@mthreads.com>
2023-03-20 19:34:49 +0800

    update data preparing script (add duration to the list)

--- tools/generate_vits_data.py
+++ tools/generate_vits_data.py
@@ -1,13 +1,18 @@
 import argparse
 import os
+import logging
 import json
 import yaml
 import librosa
 import tgt
+import torch
 import pandas as pd
 from collections import defaultdict
 import numpy as np
 from scipy.io import wavfile
+from utils import load_wav_to_torch, combine_list
+from mel_processing import spectrogram_torch
+from multiprocessing import Process
 
 
 sil_phones = ["sil", "sp", "spn", "sp1", "sp2", "sp3", "sp0", "spb"]
@@ -44,7 +49,26 @@ def stats_sil_phones_from_path(path):
     return spk2sos_durations, spk2eos_durations, spk2sil_durations
 
 
-def get_phones(between_words, preprocess_config, spk2pause_stats, tg_name):
+def get_phones(between_words, sample_rate, hop_length, spk2pause_stats, tg_name):
+    '''
+    Output the phone sequence and the duration of each phone.
+    Also the actual start and end time of speech is marked.
+    NOTE: 
+    The duration depends on the parameter in "preprocess_config".
+    If we change the config (sample rate, frame length, hop length, etc.), the duration should be 
+    generated again!
+
+    Args:
+        between_words: If true, we manually add blank between words (not phones)
+        preprocess_config: The config is similar to the one used in FastSpeech
+        spk2pause_stats: help us to convert silence into different levels
+        tg_name: the input TextGrid file
+    
+    Returns:
+        phones: the converted phone sequence
+        durations: the duration of each phone
+        start_time, end_time: the start and end time of the speech
+    '''
     textgrid = tgt.io.read_textgrid(tg_name, include_empty_intervals=True)
 
     # map the words and the corresponding phones
@@ -106,12 +130,8 @@ def get_phones(between_words, preprocess_config, spk2pause_stats, tg_name):
 
             durations.append(
                 int(
-                    np.round(
-                        e * preprocess_config["preprocessing"]["audio"]["sampling_rate"] /
-                        preprocess_config["preprocessing"]["stft"]["hop_length"])
-                    - np.round(
-                        s * preprocess_config["preprocessing"]["audio"]["sampling_rate"] /
-                        preprocess_config["preprocessing"]["stft"]["hop_length"])
+                    np.round(e * sample_rate / hop_length)
+                    - np.round(s * sample_rate / hop_length)
                 )
             )
 
@@ -126,10 +146,6 @@ def get_phones(between_words, preprocess_config, spk2pause_stats, tg_name):
     # Trim tailing silences
     phones = phones[:end_idx]
     durations = durations[:end_idx]
-
-    # if not (word2phones[0][1][0][-1] == "" and word2phones[-1][1][0][-1] == ""):
-    #     import pdb
-    #     pdb.set_trace()
     return phones, durations, start_time, end_time
 
 
@@ -138,20 +154,145 @@ def output_phones(out, input_file, output_file):
         for line in fp_in:
             name = line.strip().split("|")[0]
             if name not in out:
-                print(f"Skip {name} since it is not found in the TextGrid")
+                continue
 
-            if len(out[name]) == 2:
+            if len(out[name]) == 3:
                 # without sid
-                fp_out.write('{}|{}\n'.format(out[name][0], ' '.join(out[name][1])))
+                fp_out.write('{}|{}|{}\n'.format(
+                    out[name][0],
+                    ' '.join(out[name][1]),
+                    ' '.join([str(d) for d in out[name][2]])
+                ))
             else:
                 # with sid
-                fp_out.write('{}|{}|{}\n'.format(out[name][0], out[name][2], ' '.join(out[name][1])))
-                
+                fp_out.write('{}|{}|{}|{}\n'.format(
+                    out[name][0],
+                    out[name][2],
+                    ' '.join(out[name][1]),
+                    ' '.join([str(d) for d in out[name][3]])
+                ))
+
+
+def check_list(input_text, output_text):
+    wav_set = set()
+    with open(output_text) as f:
+        for line in f:
+            name = line.strip().split("|")[0].rsplit("/", 1)[-1].rsplit(".", 1)[0]
+            wav_set.add(name)
+    with open(input_text) as f:
+        for line in f:
+            name = line.strip().split("|")[0]
+            if name not in wav_set:
+                logging.warning(f"Skip {name} since it is not found.")
+
+
+def do_work(
+    index,
+    args,
+    preprocess_config,
+    config,
+    spk2pause_stats,
+    spk2id,
+    spk_list,
+    output_wav_dir,
+    output_list_dir
+):
+    raw_path = preprocess_config["path"]["raw_path"]
+    preprocessed_path = preprocess_config["path"]["preprocessed_path"]
+
+    # Load the STFT parameters
+    max_wav_value = config["data"]["max_wav_value"] if config is not None else \
+        preprocess_config["preprocessing"]["audio"]["max_wav_value"]
+    sample_rate = config["data"]["sampling_rate"] if config is not None else \
+        preprocess_config["preprocessing"]["audio"]["sampling_rate"]
+    hop_length = config["data"]["hop_length"] if config is not None else \
+        preprocess_config["preprocessing"]["stft"]["hop_length"]
+    win_length = config["data"]["win_length"] if config is not None else \
+        preprocess_config["preprocessing"]["stft"]["win_length"]
+    filter_length = config["data"]["filter_length"] if config is not None else \
+        preprocess_config["preprocessing"]["stft"]["filter_length"]
+    
+    out = {}
+    for speaker in spk_list:
+        for tg_name in os.listdir(os.path.join(preprocessed_path, "TextGrid", speaker)):
+            if ".TextGrid" not in tg_name:
+                continue
+            basename = tg_name.rsplit(".", 1)[0]
+            wav_path = os.path.join(raw_path, speaker, "{}.wav".format(basename))
+
+            phones, duration, start, end = get_phones(
+                args.words,
+                sample_rate,
+                hop_length,
+                spk2pause_stats[speaker],
+                os.path.join(preprocessed_path, "TextGrid", speaker, tg_name)
+            )
+
+            sr, wav = wavfile.read(wav_path)
+            assert sr == sample_rate
+
+            if args.remove_silence:
+                # trim wav
+                start_time = int(sample_rate * start)
+                end_time = start_time + int(sum(duration) * hop_length)
+                # Check whether the duration is correct. The total duration should be close to the actual end time.
+                assert abs(end_time / sample_rate - end) < (hop_length / sample_rate), \
+                    "Duration wrong {} vs {}? {}".format(end_time / sample_rate, end, wav_path)
+                if end_time > wav.shape[0]:
+                    logging.warning("The length of transcription is larger than the wav: {}".format(wav_path))
+                    end_time = wav.shape[0]
+                    start_time = max(end_time - int(sum(duration) * hop_length), 0)
+                wav = wav[start_time:end_time]
+            else:
+                # add silence to the label
+                # phones = ['sil'] + phones + ['sil']
+                raise NotImplementedError("Remove silence before training")
+
+            # write wave
+            os.makedirs(os.path.join(output_wav_dir, "Wave", speaker), exist_ok=True)
+            out_wav_path = os.path.join(output_wav_dir, "Wave", speaker, "{}.wav".format(basename))
+            wavfile.write(out_wav_path, sr, wav.astype(np.int16))
+
+            # Extract spectrogram
+            audio_norm = torch.from_numpy(wav) / max_wav_value
+            audio_norm = audio_norm.unsqueeze(0)
+            spec_filename = out_wav_path.replace(".wav", ".spec.pt")
+            spec = spectrogram_torch(audio_norm, filter_length,
+                sample_rate, hop_length, win_length,
+                center=False)
+            assert spec.size(2) == sum(duration), "Mismatch: spec {} vs duration {}".format(spec.size(2), sum(duration))
+            spec = torch.squeeze(spec, 0)
+            torch.save(spec, spec_filename)
+
+            if args.use_sid:
+                out[basename] = [out_wav_path, phones, spk2id[speaker], duration]
+            else:
+                out[basename] = [out_wav_path, phones, duration]
+    
+    output_phones(
+        out,
+        os.path.join(preprocessed_path, args.train_txt_fpath),
+        os.path.join(output_list_dir, '{}.{}'.format(args.train_txt_fpath, index))
+    )
+    if args.val_txt_fpath is not None:
+        output_phones(
+            out,
+            os.path.join(preprocessed_path, args.val_txt_fpath),
+            os.path.join(output_list_dir, '{}.{}'.format(args.val_txt_fpath, index))
+        )
+    if args.test_txt_fpath is not None:
+        output_phones(
+            out,
+            os.path.join(preprocessed_path, args.test_txt_fpath),
+            os.path.join(output_list_dir, '{}.{}'.format(args.test_txt_fpath, index))
+        )
+
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--nj", type=int, default=32, help="num of workers")
     parser.add_argument("-w", "--words", action="store_true", help="add #0 between words")
-    parser.add_argument("--train_txt_fpath", type=str, default="train.txt")
+    parser.add_argument("--train_txt_fpath", type=str, required=True, default="train.txt")
     parser.add_argument("--val_txt_fpath", type=str, default=None)
     parser.add_argument("--test_txt_fpath", type=str, default=None)
     parser.add_argument("--use_sid", action="store_true", help="add speaker id to the labels")
@@ -161,15 +302,27 @@ def main():
         default=True,
         help="To remove the heading and tailing silence"
     )
-    parser.add_argument("-p", "--preprocess_config", type=str)
-    parser.add_argument("-o", "--output_dir", type=str)
+    parser.add_argument("-p", "--preprocess_config", required=True, type=str)
+    parser.add_argument("-o", "--output_dir", required=True, type=str)
+    parser.add_argument("-c", "--config", default=None, type=str, help="The config used in vits training")
+    parser.add_argument("-l", "--output_list_dir", default=None, type=str, help="The output dir of the list")
     args = parser.parse_args()
+
+    # Load the speaker info
+    config = None
+    if args.config is not None:
+        with open(args.config, "r") as f:
+            data = f.read()
+        config = json.loads(data)
 
     preprocess_config = yaml.load(
         open(args.preprocess_config, "r"), Loader=yaml.FullLoader
     )
-    raw_path = preprocess_config["path"]["raw_path"]
     preprocessed_path = preprocess_config["path"]["preprocessed_path"]
+    output_wav_dir = args.output_dir
+    output_list_dir = args.output_list_dir if args.output_list_dir is not None else output_wav_dir
+    os.makedirs(output_wav_dir, exist_ok=True)
+    os.makedirs(output_list_dir, exist_ok=True)
 
     spk_dirs = [
         speaker for speaker in os.listdir(os.path.join(preprocessed_path, "TextGrid"))
@@ -181,14 +334,17 @@ def main():
     print("Total num of spks: {}".format(len(spk_dirs)))
 
     if args.use_sid:
-        if "speakers_path" not in preprocess_config["path"]:
+        if "speakers_path" in preprocess_config["path"]:
+            spk2id = json.load(open(preprocess_config["path"]["speakers_path"]))
+        elif os.path.exists(os.path.join(preprocessed_path, "speakers.json")):
+            spk2id = json.load(open(os.path.join(preprocessed_path, "speakers.json")))
+        else:
             spk2id = {}
             for sid, speaker in enumerate(spk_dirs):
                 spk2id[speaker] = sid
-        else:
-            spk2id = json.load(open(preprocess_config["path"]["speakers_path"]))
-
     print(spk2id)
+
+    # Compute the stats of silence of each speaker
     sos_sil_stat, eos_sil_stat, sil_stat = stats_sil_phones_from_path(preprocess_config["path"]["preprocessed_path"])
     spk2pause_stats = dict()
     for spk in sos_sil_stat.keys():
@@ -196,64 +352,45 @@ def main():
         pos_stats = pd.Series(sil_stat[spk]).describe(percentiles=[.3, .6, .8, .99])
         spk2pause_stats[spk] = [pos_stats["30%"], pos_stats["60%"], pos_stats["80%"]]
 
-    out = {}
-    for speaker in spk_dirs:
-        for tg_name in os.listdir(os.path.join(preprocessed_path, "TextGrid", speaker)):
-            if ".TextGrid" not in tg_name:
-                continue
-            basename = tg_name.split(".")[0]
-            wav_path = os.path.join(raw_path, speaker, "{}.wav".format(basename))
-
-            phones, duration, start, end = get_phones(
-                args.words,
-                preprocess_config,
-                spk2pause_stats[speaker],
-                os.path.join(preprocessed_path, "TextGrid", speaker, tg_name)
-            )
-
-            sr, wav = wavfile.read(wav_path)
-            assert sr == preprocess_config["preprocessing"]["audio"]["sampling_rate"]
-
-            if args.remove_silence:
-                # trim wav
-                wav = wav[
-                    max(int(preprocess_config["preprocessing"]["audio"]["sampling_rate"] * start), 0) :
-                        int(preprocess_config["preprocessing"]["audio"]["sampling_rate"] * end)
-                ]
-            else:
-                # add silence to the label
-                phones = ['sil'] + phones + ['sil']
-                
-            # write wave
-            os.makedirs(os.path.join(args.output_dir, "Wave", speaker), exist_ok=True)
-            out_wav_path = os.path.join(args.output_dir, "Wave", speaker, "{}.wav".format(basename))
-            wavfile.write(out_wav_path, sr, wav.astype(np.int16))
-            if args.use_sid:
-                out[basename] = [out_wav_path, phones, spk2id[speaker]]
-            else:
-                out[basename] = [out_wav_path, phones]
-    
-    output_phones(
-        out,
-        os.path.join(preprocessed_path, args.train_txt_fpath),
-        os.path.join(args.output_dir, args.train_txt_fpath)
-    )
-    if args.val_txt_fpath is not None:
-        output_phones(
-            out,
-            os.path.join(preprocessed_path, args.val_txt_fpath),
-            os.path.join(args.output_dir, args.val_txt_fpath)
-        )
-    if args.test_txt_fpath is not None:
-        output_phones(
-            out,
-            os.path.join(preprocessed_path, args.test_txt_fpath),
-            os.path.join(args.output_dir, args.test_txt_fpath)
-        )
-
     if args.use_sid:
-        with open(os.path.join(args.output_dir, "speakers.json"), 'w') as f:
+        with open(os.path.join(output_list_dir, "speakers.json"), 'w') as f:
             json.dump(spk2id, f)
+
+    split_spk_list = [[] for _ in range(args.nj)]
+    for i, speaker in enumerate(spk_dirs):
+        split_spk_list[i % args.nj].append(speaker)
+
+    # i = 0
+    # do_work(
+    #     i, args, preprocess_config, config, spk2pause_stats, spk2id,
+    #     split_spk_list[i], output_wav_dir, output_list_dir
+    # )
+    # quit()
+
+    processes = [Process(
+        target=do_work, args=(
+            i, args, preprocess_config, config, spk2pause_stats, spk2id,
+            split_spk_list[i], output_wav_dir, output_list_dir
+        )
+    ) for i in range(args.nj)]
+    for proc in processes:
+        proc.daemon = True
+        proc.start()
+    for proc in processes:
+        proc.join()
+
+    # combine text
+    combine_list(args.nj, os.path.join(output_list_dir, args.train_txt_fpath))
+    check_list(os.path.join(preprocessed_path, args.train_txt_fpath), os.path.join(output_list_dir, args.train_txt_fpath))
+    os.system("rm {}".format(" ".join([os.path.join(output_list_dir, "{}.{}".format(args.train_txt_fpath, i)) for i in range(args.nj)])))
+    if args.val_txt_fpath is not None:
+        combine_list(args.nj, os.path.join(output_list_dir, args.val_txt_fpath))
+        check_list(os.path.join(preprocessed_path, args.val_txt_fpath), os.path.join(output_list_dir, args.val_txt_fpath))
+        os.system("rm {}".format(" ".join([os.path.join(output_list_dir, "{}.{}".format(args.val_txt_fpath, i)) for i in range(args.nj)])))
+    if args.test_txt_fpath is not None:
+        combine_list(args.nj, os.path.join(output_list_dir, args.test_txt_fpath))
+        check_list(os.path.join(preprocessed_path, args.test_txt_fpath), os.path.join(output_list_dir, args.test_txt_fpath))
+        os.system("rm {}".format(" ".join([os.path.join(output_list_dir, "{}.{}".format(args.test_txt_fpath, i)) for i in range(args.nj)])))
 
 
 if __name__ == '__main__':
